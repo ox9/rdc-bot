7dca7dd73d52d4950abd25526c7f3d82bcdd5cae
ox9 <ox9git@gmail.com>
2020-08-11 16:15:49 -0700

    Added guild command

---
+++ src/standalone/guild.js
@@ -0,0 +1,46 @@
+import { Command } from "../core/command";
+import { Message } from "discord.js";
+
+export class Guild extends Command {
+    constructor() {
+        super(null);
+    }
+    
+    get name() {
+        return "Guild";
+    }
+    
+    get prefix() {
+        return "guild";
+    }
+
+    get syntax() {
+        return "<none>";
+    }
+    
+    get description() {
+        return "Gives information about the guild";
+    }
+    
+    /**
+    *
+    * @param {Message} msg
+    */
+    onInvoked(msg) {
+        const guild = msg.guild;
+        message.channel.send({
+            embed: {
+                color: "#26aafc",
+                title: guild.name,
+                description: guild.name,
+                fields: [
+                    { name: "Server ID", value: guild.id },
+                    { name: "Member Count", value: guild.memberCount },
+                    { name: "Description", value: guild.description },
+                    { name: "Creation Date", value: guild.createdAt },
+                    { name: "Owner", value: guild.owner },
+                ],
+            },
+        });
+    }
+}
