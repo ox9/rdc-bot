54410910d168d847062837ca261483b19540b370
ox9 <ox9git@gmail.com>
2020-08-10 19:53:15 -0700

    Changed to ES6 Babel

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+import { Client } from 'discord.js';
+const client = new Client();
 
 require('dotenv').config();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	if (msg.content === 'ping') {
+		msg.reply('Pong!');
+	}
 });
 
 client.login(process.env.TOKEN);
