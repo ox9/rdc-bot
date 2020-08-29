1cfb0736d6e00683b0226a7b6803a845c0ed899f
ox9 <ox9git@gmail.com>
2020-08-29 00:39:05 -0700

    Added shortcuts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { Client } from 'discord.js';
 import { CONFIG } from './config';
 import { REGISTER } from './register';
+import fs from 'fs';
 
 console.log(CONFIG);
+
+let shortcuts;
+fs.readFile("./config/shortcuts.json", (err, data) => { 
+	if (err) throw err; 
+
+    shortcuts = JSON.parse(data); 
+}); 
 
 const client = new Client();
 
@@ -13,8 +21,21 @@ client.on('ready', () => {
 client.on('message', msg => {
 	if (!msg.content.startsWith(CONFIG.BOT_PREFIX)) { return; }
 
+	const cutTxt = msg.content.substring(1);
+
+	for (const shortcut of shortcuts) {
+		if (cutTxt === shortcut.cue || Array.isArray(shortcut.cue) && shortcut.cue.includes(cutTxt)) {
+			msg.channel.send({
+				embed: {
+					description: shortcut.answer
+				}
+			});
+			return;
+		}
+	}
+
 	for (const cmd of REGISTER.REGISTERED_CMDS) {
-		const retval = cmd.isInvoked(msg.content.substring(1));
+		const retval = cmd.isInvoked(cutTxt);
 		if (retval !== false) {
 			msg.trimed = retval;
 			cmd.onInvoked(msg);
